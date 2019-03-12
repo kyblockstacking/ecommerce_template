28d00e5e4ee8d842f941a1dec0b146a5795b72f7
kyblockstacking <kevin1992yang@gmail.com>
2019-03-11 18:05:50 -0700

    navbar content

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -6,8 +6,8 @@ class Navbar extends Component {
             <nav className='navbar'>
 
                 <div className='topNavbar' style={{ margin: '0 auto', width: '90vw', borderBottom: '1px solid black', height: '10vh', display: 'flex', alignItems: ' center' }}>
-                    <h1 style={{ marginRight: '20%', width: '40%' }}>DANK POTS</h1>
-                    <h1 style={{ width: '40%', textAlign: 'right', justifyContent: 'flex-end' }}>Kevin Yang</h1>
+                    <a href='/' style={{ marginRight: '20%', width: '40%', textDecoration: 'none', }}>DANK POTS</a>
+                    <a href='https://github.com/kyblockstacking/' target='_blank' style={{ width: '40%', textAlign: 'right', justifyContent: 'flex-end', textDecoration: 'none' }}>Kevin Yang</a>
                 </div>
 
                 <div className='bottomNavbar' style={{ display: 'flex', flexDirection: 'row', margin: '0 auto', width: '90vw' }}>
