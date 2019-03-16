240c188ceb7a5bfa05a1165d6140ab7593dc6f56
kyblockstacking <kevin1992yang@gmail.com>
2019-03-16 14:57:41 -0700

    move styling into scss

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -6,18 +6,18 @@ class Navbar extends Component {
 
     render() {
         return (
-            <nav className='navbar' style={styles.navbar}>
+            <nav className='navbar'>
 
-                <div className='topNavbar' style={styles.topNavbar}>
-                    <a className='brand' href='/' style={styles.topNavbar.brand}>DANK POTS</a>
-                    <a className='name' href='https://github.com/kyblockstacking/' target='_blank' style={styles.topNavbar.name}>
-                        <a className='cart' href='#' className='fas fa-shopping-cart' style={styles.cart}>&nbsp;(1)&nbsp;</a>
+                <div className='topNavbar'>
+                    <a className='brand' href='/'>DANK POTS</a>
+                    <a className='name' href='https://github.com/kyblockstacking/' target='_blank'>
+                        <a className='fas fa-shopping-cart cart' href='#'>&nbsp;(1)&nbsp;</a>
                         Kevin Yang
                     </a>
                 </div>
 
-                <div className='bottomNavbar' style={styles.bottomNavBar}>
-                    <div className='subShopLabel' style={styles.bottomNavBar.subShopLabel}>SHOP</div>
+                <div className='bottomNavbar'>
+                    <div className='subShopLabel'>SHOP</div>
                     <a className='navbarLinks' href='#'>Plants</a>
                     <a className='navbarLinks' href='#'>Pots</a>
                     <a className='navbarLinks' href='#'>Accessories</a>
@@ -27,47 +27,5 @@ class Navbar extends Component {
         );
     };
 };
-
-const styles = {
-    navbar: {
-        position: 'fixed',
-        marginLeft: '5vw'
-    },
-    topNavbar: {
-        margin: '0 auto',
-        width: '90vw',
-        borderBottom: '1px solid black',
-        height: '10vh',
-        display: 'flex',
-        alignItems: ' center',
-
-        brand: {
-            textDecoration: 'none',
-            color: '#1C572F'
-        },
-        name: {
-            marginLeft: 'auto',
-            textAlign: 'right',
-            justifyContent: 'flex-end',
-            textDecoration: 'none'
-        }
-    },
-    bottomNavBar: {
-        display: 'flex',
-        flexDirection: 'row',
-        margin: '0 auto',
-        width: '90vw',
-        subShopLabel: {
-            margin: '0 1.5em 0 0',
-            padding: '0 10px 0 10px',
-            borderBottom: '1px solid black',
-            borderRight: '1px solid black',
-            borderLeft: '1px solid black',
-        }
-    },
-    cart: {
-        textDecoration: 'none'
-    }
-}
 
 export default Navbar;
