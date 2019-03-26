2586f666d77fba691aec295a024955d675728e03
kyblockstacking <kevin1992yang@gmail.com>
2019-03-25 17:18:38 -0700

    add react-bootstrap

---
+++ src/Components/HotDealsCarousel.jsx
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import Carousel from 'react-bootstrap/Carousel'
+import '../CSS/HotDealsCarousel.scss';
+
+import deal1 from '../Images/deal1.jpg';
+import deal2 from '../Images/deal2.jpg';
+import deal3 from '../Images/deal3.jpg';
+
+class HotDealsCarousel extends Component {
+    render() {
+        return (
+            <Carousel className='hotDealsCarousel'>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100 hotDealsImage"
+                        src={deal1}
+                        alt="First slide"
+                    />
+                    <Carousel.Caption>
+                        <h3>First slide label</h3>
+                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100 hotDealsImage"
+                        src={deal2}
+                        alt="Second slide"
+                    />
+
+                    <Carousel.Caption>
+                        <h3>Second slide label</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100 hotDealsImage"
+                        src={deal3}
+                        alt="Third slide"
+                    />
+
+                    <Carousel.Caption>
+                        <h3>Third slide label</h3>
+                        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+            </Carousel>
+        )
+    }
+}
+
+export default HotDealsCarousel;
--- src/Components/Shop.jsx
+++ src/Components/Shop.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import HotDealsCarousel from '../Components/HotDealsCarousel.jsx'
 import '../CSS/Shop.scss';
 
 class Shop extends Component {
@@ -7,6 +7,8 @@ class Shop extends Component {
         return (
             <div className='shop'>
                 <h1 class='shopTitle'>DANK POT'S SHOP</h1>
+                <h2>Smokin' Hot Deals</h2>
+                <HotDealsCarousel />
             </div>
         );
     };
