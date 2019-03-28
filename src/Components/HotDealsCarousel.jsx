a8ed0a10671092b923cbf981d17b0d35e308d7f1
kyblockstacking <kevin1992yang@gmail.com>
2019-03-28 14:13:12 -0700

    add dummy shop items

--- src/Components/HotDealsCarousel.jsx
+++ src/Components/HotDealsCarousel.jsx
@@ -6,46 +6,57 @@ import deal1 from '../Images/deal1.jpg';
 import deal2 from '../Images/deal2.jpg';
 import deal3 from '../Images/deal3.jpg';
 
+import FeatureBox from '../Components/Reusable/FeatureBox.jsx';
+
 class HotDealsCarousel extends Component {
     render() {
         return (
-            <Carousel className='hotDealsCarousel'>
-                <Carousel.Item>
-                    <img
-                        className='d-block w-100 hotDealsImage'
-                        src={deal1}
-                        alt='First slide'
-                    />
-                    <Carousel.Caption>
-                        <h3>First slide label</h3>
-                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item>
-                    <img
-                        className='d-block w-100 hotDealsImage'
-                        src={deal2}
-                        alt='Second slide'
-                    />
+            <div class='shopPage'>
+                <Carousel className='hotDealsCarousel'>
+                    <Carousel.Item>
+                        <img
+                            className='d-block w-100 hotDealsImage'
+                            src={deal1}
+                            alt='First slide'
+                        />
+                        <Carousel.Caption>
+                            <h3>First slide label</h3>
+                            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img
+                            className='d-block w-100 hotDealsImage'
+                            src={deal2}
+                            alt='Second slide'
+                        />
 
-                    <Carousel.Caption>
-                        <h3>Second slide label</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item>
-                    <img
-                        className='d-block w-100 hotDealsImage'
-                        src={deal3}
-                        alt='Third slide'
-                    />
+                        <Carousel.Caption>
+                            <h3>Second slide label</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img
+                            className='d-block w-100 hotDealsImage'
+                            src={deal3}
+                            alt='Third slide'
+                        />
 
-                    <Carousel.Caption>
-                        <h3>Third slide label</h3>
-                        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-            </Carousel>
+                        <Carousel.Caption>
+                            <h3>Third slide label</h3>
+                            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                </Carousel>
+
+                <div class='shopItems'>
+                    <FeatureBox />
+                    <FeatureBox />
+                    <FeatureBox />
+                </div>
+
+            </div>
         )
     }
 }
