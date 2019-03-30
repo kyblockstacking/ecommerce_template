9b1cf251c18631c0616ba7f619321d3d3a348c01
kyblockstacking <kevin1992yang@gmail.com>
2019-03-30 14:14:44 -0700

    shop pots pages adjustments

--- src/Components/ShopPots.jsx
+++ src/Components/ShopPots.jsx
@@ -8,23 +8,17 @@ class ShopPots extends Component {
     render() {
         return (
             <div className='ShopPots'>
-                <h2 className='potsTitle'>Pots</h2>
+                <h2 className='ShopPotsTitle'>Pots</h2>
                 <div className='potsItemSection'>
                     <ShopItem title='pot1' description='description' src={pot1} />
                     <ShopItem title='pot2' description='description' src={pot1} />
                     <ShopItem title='pot3' description='description' src={pot1} />
-                    <ShopItem title='pot1' description='description' src={pot1} />
-                    <ShopItem title='pot2' description='description' src={pot1} />
-                    <ShopItem title='pot3' description='description' src={pot1} />
-                    <ShopItem title='pot1' description='description' src={pot1} />
-                    <ShopItem title='pot2' description='description' src={pot1} />
-                    <ShopItem title='pot3' description='description' src={pot1} />
-                    <ShopItem title='pot1' description='description' src={pot1} />
-                    <ShopItem title='pot2' description='description' src={pot1} />
-                    <ShopItem title='pot3' description='description' src={pot1} />
-                    <ShopItem title='pot1' description='description' src={pot1} />
-                    <ShopItem title='pot2' description='description' src={pot1} />
-                    <ShopItem title='pot3' description='description' src={pot1} />
+                    <ShopItem title='pot4' description='description' src={pot1} />
+                    <ShopItem title='pot5' description='description' src={pot1} />
+                    <ShopItem title='pot6' description='description' src={pot1} />
+                    <ShopItem title='pot7' description='description' src={pot1} />
+                    <ShopItem title='pot8' description='description' src={pot1} />
+                    <ShopItem title='pot9' description='description' src={pot1} />
                 </div>
             </div>
 
